d4089b60f47373193af0f22f266e2441c1efa478
Luis Furnas <luisfurnas@gmail.com>
2018-10-05 18:22:35 +0000

    Added firebase and material packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule, MatSidenavModule, MatListModule, MatMenuModule, MatIconModule} from '@angular/material';
 import {AngularFireModule} from 'angularfire2';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {MaterialModule} from './material/material.module';
-import {environment} from '../environments/environment';
+import {firebaseConf} from '../environments/api-keys';
 import {LoginComponent} from './login/login.component';
 import {MainComponent} from './main/main.component';
 
@@ -16,8 +15,7 @@ import {MainComponent} from './main/main.component';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    MatToolbarModule, MatSidenavModule, MatListModule, MatMenuModule, MatIconModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp(firebaseConf),
     AppRoutingModule
   ],
   declarations: [
